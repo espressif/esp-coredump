3da23276aad6da7fab693dc2bcf05f5dfbd08b94
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2023-03-29 16:57:55 +0800

    tests: get rid of the following targets in tests - 'esp32s2', 'esp32s3', 'esp32c2'

--- tests/test_espcoredump.py
+++ tests/test_espcoredump.py
@@ -15,7 +15,7 @@ except ImportError:
     raise ModuleNotFoundError('No module named "esp_coredump" please install esp_coredump by running '
                               '"python -m pip install esp-coredump"')
 
-SUPPORTED_TARGET = ['esp32', 'esp32s2', 'esp32c3', 'esp32s3', 'esp32c2']
+SUPPORTED_TARGET = ['esp32', 'esp32c3']
 APP_TYPE = ['with_rom', 'without_rom']
 COREDUMP_FILE_NAME = 'coredump'
 
